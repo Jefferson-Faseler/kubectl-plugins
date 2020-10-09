dd685a4f2eace8d7f3dbf9e72ac854acb2f5026d
Jefferson Faseler <jjfaseler@gmail.com>
2020-10-09 00:39:01 -0500

    Remove profile dependency (#2)

--- install-plugins.sh
+++ install-plugins.sh
@@ -7,14 +7,21 @@ echo "Installing kubectl-plugins to ~/${SHELL_PROFILE}"
 
 # Move plugins and add location to path (required for kubectl 1.12+)
 chmod +x ./kubectl-*
-rm -rf ~/.kube/plugins/jordanwilson230
-mkdir -p ~/.kube/plugins/jordanwilson230
+rm -rf ~/.kube/plugins/jordanwilson230/kubectl-*
+mkdir -p ~/.kube/plugins/jordanwilson230/config
+touch ~/.kube/plugins/jordanwilson230/config/namespaces
+touch ~/.kube/plugins/jordanwilson230/config/clusters
 cp -r ./kubectl-* ~/.kube/plugins/jordanwilson230/
 grep 'PATH=$PATH:~/.kube/plugins/jordanwilson230' ~/${SHELL_PROFILE} 1>/dev/null
 [ $? -ne 0 ] && echo 'export PATH=$PATH:~/.kube/plugins/jordanwilson230' >> ~/${SHELL_PROFILE}
 
+
 # Removes old installer function if exists
 ex '+g/function kubectl()/d' -cwq ~/${SHELL_PROFILE} >/dev/null 2>&1
+
+# Add function to check user context when kubectl is invoked.
+ex '+g/function kubectl/d' -cwq ~/${SHELL_PROFILE}
+echo 'function kubectl() { command kubectl prompt "$@" && command kubectl "$@" }' >> ~/${SHELL_PROFILE}
 
 # Finished
 echo -e "\nDone.\nPlease open a new terminal or run: source ~/${SHELL_PROFILE}\n"
